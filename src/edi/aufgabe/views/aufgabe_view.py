1a08dd75f2cc7a3de22e3235f1d6ee7969e4b4d6
Lars Walther <lwalther@novareto.de>
2022-03-05 18:11:21 +0100

    aufgabe_view.py entwickelt

--- src/edi/aufgabe/views/aufgabe_view.py
+++ src/edi/aufgabe/views/aufgabe_view.py
@@ -1,17 +1,98 @@
 # -*- coding: utf-8 -*-
 
-from edi.aufgabe import _
-from Products.Five.browser import BrowserView
+from wtforms import Form, TextField
+from wtforms.validators import InputRequired
+from collective.wtforms.views import WTFormView
+from wtforms.fields import MultipleFileField
+from plone import api as ploneapi
+from plone.app.textfield.value import RichTextValue
 
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+class SolutionForm(Form):
+    text = TextAreaField("Beschreibung der Lösung" [InputRequired()])
+    files = MultipleFileField("Upload von Dateien")
 
 
-class AufgabeView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('aufgabe_view.pt')
+class AufgabeView(WTFormView):
+    formClass = MyForm
+    buttons = ('Speichern', 'Abbrechen')
 
     def __call__(self):
-        # Implement your own actions:
-        self.msg = _(u'A small message')
+        self.current = None
+        self.homefolder = None
+        self.authenticated = False
+        membership = ploneapi.portal.get_tool(name='portal_membership')
+        if not ploneapi.user.is_anonymous():
+            self.authenticated = True
+            self.current = ploneapi.user.get_current()
+            self.homefolder = membership.getHomeFolder(current.getId())
+        self.solution = self.get_solution()
+        self.groupsolutions = self.get_group_solutions()
         return self.index()
+
+    def get_solution(self):
+        if not self.homefolder:
+            return
+        aufgabeuid = self.context.UID()
+        meineaufgabe_id = f'meine_aufgabe_{aufgabeuid}' 
+        if meineaufgabe_id in self.homefolder:
+            solution = {}
+            meineaufgabe = self.homefolder[meineaufgabe_id]
+            if meineaufgabe.text:
+                solution['text'] = meineaufgabe.text.output
+            brains = meineaufgabe.getFolderContents()
+            if brains:
+                solution['files'] = []
+                for i in brains:
+                    fileentry = {}
+                    if i.portal_type == 'File':
+                        fileentry['title'] = i.Title
+                        fileentry['url'] = i.getURL()
+                        solution['files'].append(fileentry)
+            return solution
+        return
+
+    def get_group_solutions(self):
+        if not self.authenticated:
+            return
+        aufgabeuid = self.context.UID()
+        meineaufgabe_id = f'meine_aufgabe_{aufgabeuid}'
+        brains = ploneapi.content.find(portal_type='MeineAufgabe', id=meineaufgabe_id)
+        group_solutions = []
+        for brain in brains:
+            group_entry = {}
+            meineaufgabe = brain.getObject()
+            roles = ploneapi.user.get_roles(user=self.current, obj=meineaufgabe)
+            if not 'Owner' in roles:
+                group_entry['title'] = meineaufgabe.getOwner().getProperty('fullname')
+                group_entry['url'] = meineaufgabe.absolute_url()
+                group_entry['text'] = ''
+                if meineaufgabe.text:
+                    group_entry['text'] = meineaufgabe.text.output
+                group_entry['files'] = []
+                for fileentry = meineaufgabe.getFolderContents()
+                    file_entry = {}
+                    if fileentry.portal_type == 'File':
+                        file_entry['title'] = fileentry.Title
+                        file_entry['url'] = fileentry.getURL()
+                        group_entry['files'].append(file_entry)
+                group_solutions.append(group_entry)
+        return group_solutions        
+
+
+    def submit(self, button):
+        if button == 'Speichern' and self.validate():
+             if not self.homefolder:
+                 message = 
+                 return
+             aufgabeuid = self.context.UID()
+             meineaufgabe_id = f'meine_aufgabe_{aufgabeuid}'
+             richtext = RichTextValue(raw=self.form.text.data,
+                          mimeType='text/plain',
+                          outputMimeType='text/html',
+                          encoding='utf-8')
+             obj = ploneapi.content.create(
+                      type='MeineAufgabe',
+                      id = meineaufgabe_id,
+                      title=self.context.title,
+                      container=self.homefolder)
+             import pdb;pdb.set_trace()
